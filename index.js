7c4d38663019f0d4a7766e7f6055bd31b21d36af
siddharthasarma <sidduinevitable@gmail.com>
2019-01-17 17:36:14 +0530

    Changed var to const and added promises

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const inputFilePath = './prices.csv';
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
   user: 'newuser',
   password: 'password',
   database: 'stocks_db'
 });
 connection.connect();
-var records = [];
-var i = 0;
+const records = [];
+let i = 0;
 fs.createReadStream(inputFilePath)
   .pipe(csv())
   .on('data', function(data) {
     try {
-      var date = data.date.split(' ')[0];
+      let date = data.date.split(' ')[0];
       // console.log(date);
-      // date = date.split('-');
-      // var formattedDate = `${date[2]}-${date[1]}-${date[0]}`;
+      date = date.split('-');
+      date = `${date[2]}-${date[1]}-${date[0]}`;
       records.push([
         date,
         data.symbol,
@@ -38,17 +39,23 @@ fs.createReadStream(inputFilePath)
   });
 
 const dumpInToDB = async () => {
-  var sql =
+  const promises = [];
+  const sql =
     'INSERT INTO `stocks_demo2_tb` (`date`,`symbol`, `open`, `close`, `low`, `high`, `volume`) VALUES ?';
 
   records.shift();
-  var recordBreak = 100000;
-  for (var i = 0; i < Math.ceil(records.length / recordBreak); i++) {
-    var recordsList = records.slice(i * recordBreak, (i + 1) * recordBreak);
-    await connection.query(sql, [recordsList], function(err) {
-      if (err) throw err;
-      console.log('hey');
-    });
+  const recordBreak = 10;
+  const maxIterations = Math.ceil(records.length / recordBreak);
+  for (let i = 0; i < maxIterations; i++) {
+    const recordsList = records.slice(i * recordBreak, (i + 1) * recordBreak);
+    try {
+      promises.push(await connection.query(sql, [recordsList]));
+      // console.log(result);
+    } catch (err) {
+      console.err(err);
+    }
   }
+  await Promise.all(promises);
+  connection.end();
   console.log('done');
 };
